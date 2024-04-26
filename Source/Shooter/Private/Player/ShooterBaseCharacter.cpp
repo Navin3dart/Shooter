257be7663eb9f91fd9e122062d9b55de89380a68
Navin3dart <80153946+Navin3dart@users.noreply.github.com>
2024-04-26 21:08:29 +0300

    movement 0.1

--- Source/Shooter/Private/Player/ShooterBaseCharacter.cpp
+++ Source/Shooter/Private/Player/ShooterBaseCharacter.cpp
@@ -2,6 +2,13 @@
 
 
 #include "Player/ShooterBaseCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogShooter, All, All)
 
 // Sets default values
 AShooterBaseCharacter::AShooterBaseCharacter()
@@ -9,12 +16,24 @@ AShooterBaseCharacter::AShooterBaseCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+    SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
+    SpringArmComponent->SetupAttachment(GetRootComponent());
+    SpringArmComponent->bUsePawnControlRotation = true;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
+    CameraComponent->SetupAttachment(SpringArmComponent);
+
 }
+
 
 // Called when the game starts or when spawned
 void AShooterBaseCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+    CurrentSpeed = 300.0f;
+    TargetSpeed = 300.0f;
+    InterpSpeed = 4.0f;
+    //ErrorTolerance = 1.0f;
 	
 }
 
@@ -22,6 +41,12 @@ void AShooterBaseCharacter::BeginPlay()
 void AShooterBaseCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+    if (!(FMath::IsNearlyEqual(TargetSpeed, CurrentSpeed, 0.1)))
+    {
+        CurrentSpeed = FMath::FInterpTo(CurrentSpeed, TargetSpeed, DeltaTime, InterpSpeed);
+        GetCharacterMovement()->MaxWalkSpeed = CurrentSpeed;
+        //UE_LOG(LogShooter, Display, TEXT("Tick"));
+    }
 
 }
 
@@ -30,5 +55,38 @@ void AShooterBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis("MoveForward", this, &AShooterBaseCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &AShooterBaseCharacter::MoveRight);
+    PlayerInputComponent->BindAxis("LookUp", this, &AShooterBaseCharacter::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis("TurnAround", this, &AShooterBaseCharacter::AddControllerYawInput);
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AShooterBaseCharacter::Jump);
+    PlayerInputComponent->BindAction("Run", IE_Pressed, this, &AShooterBaseCharacter::Run);
+    PlayerInputComponent->BindAction("Walk", IE_Released, this, &AShooterBaseCharacter::Walk);
 }
 
+void AShooterBaseCharacter::MoveForward(float Amount) 
+{
+    AddMovementInput(GetActorForwardVector(), Amount);
+}
+
+void AShooterBaseCharacter::MoveRight(float Amount) 
+{
+    AddMovementInput(GetActorRightVector(), Amount);
+}
+
+void AShooterBaseCharacter::Run() 
+{
+    if (!GetVelocity().IsZero())
+    {
+        UE_LOG(LogShooter, Display, TEXT("Run"));
+        TargetSpeed = 600.0f;
+        //GetCharacterMovement()->MaxWalkSpeed = 600;
+    }
+}
+
+void AShooterBaseCharacter::Walk() 
+{
+    UE_LOG(LogShooter, Display, TEXT("Walk"));
+    TargetSpeed = 300.0f;
+    //GetCharacterMovement()->MaxWalkSpeed = 300;
+}
--- Source/Shooter/Public/Player/ShooterBaseCharacter.h
+++ Source/Shooter/Public/Player/ShooterBaseCharacter.h
@@ -6,24 +6,46 @@
 #include "GameFramework/Character.h"
 #include "ShooterBaseCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class SHOOTER_API AShooterBaseCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	AShooterBaseCharacter();
+    // Sets default values for this character's properties
+    AShooterBaseCharacter();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    USpringArmComponent* SpringArmComponent;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    UCameraComponent* CameraComponent;
+
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+
+    float CurrentSpeed;
+    float TargetSpeed;
+    float InterpSpeed;
+
+    void MoveForward(float Amount);
+    void MoveRight(float Amount);
+    void Run();
+    void Walk();
+
 
 };
