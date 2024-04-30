21926bcb105600f80c432909055798652f5e06dd
Navin3dart <80153946+Navin3dart@users.noreply.github.com>
2024-04-30 17:17:31 +0300

    pickup base

---
+++ Source/Shooter/Public/Pickups/ShooterHealthPickup.h
@@ -0,0 +1,17 @@
+// ShooterGame. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pickups/ShooterBasePickup.h"
+#include "ShooterHealthPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API AShooterHealthPickup : public AShooterBasePickup
+{
+	GENERATED_BODY()
+	
+};
